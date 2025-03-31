774038aebc88cd94af33e2e8cf40d350acc1d18d
Balthasar Hofer <lebalz@outlook.com>
2025-03-31 14:17:48 +0200

    Update: docs/00-G-Beispiel/ColorBeispiel/ColorBox/index.jsx

--- docs/00-G-Beispiel/ColorBeispiel/ColorBox/index.jsx
+++ docs/00-G-Beispiel/ColorBeispiel/ColorBox/index.jsx
@@ -1,1 +1,22 @@
+import React from "react";
+import styles from "./styles.module.css";
 
+const ColorBox = ({ nr }) => {
+  const [color, setColor] = React.useState("green");
+  return (
+    <div
+      className={styles.colorBox}
+      style={{ background: color }}
+      onClick={() => {
+        const r = Math.random() * 255;
+        const g = Math.random() * 255;
+        const b = Math.random() * 255;
+        setColor(`rgb(${r},${g},${b})`);
+      }}
+    >
+      ColorBox {nr}
+    </div>
+  );
+};
+
+export default ColorBox;
