db0042c8a56adb33913d4010b953db473b8d291f
LeBalz <lebalz@outlook.com>
2025-03-31 06:53:34 +0200

    move games to components

--- src/components/Games/CookieClicker/index.tsx
+++ src/components/Games/CookieClicker/index.tsx
@@ -5,24 +5,23 @@ import styles from './styles.module.scss';
 const CookieClicker = () => {
     const [score, setScore] = React.useState(0);
     return (
-        <div className='card'>
-            <div className={clsx(styles.score, 'card__header')}><h4>Score: {score}</h4></div>
+        <div className="card">
+            <div className={clsx(styles.score, 'card__header')}>
+                <h4>Score: {score}</h4>
+            </div>
             <div className={clsx('card__body', styles.cookie)}>
-                <div onClick={() => setScore((s) => s + 1)} >
+                <div onClick={() => setScore((s) => s + 1)}>
                     <img
-                        src={require('./cookie.png').default}
+                        src={require('./images/cookie.png').default}
                         style={{
-                            transform: `rotate(${score * 30}deg)`,
+                            transform: `rotate(${score * 30}deg)`
                         }}
                         className={styles.image}
                     />
                 </div>
             </div>
             <div className={'card__footer'}>
-                <button
-                    onClick={() => setScore(0)}
-                    className="button button--primary button--block"
-                >
+                <button onClick={() => setScore(0)} className="button button--primary button--block">
                     Zurücksetzen
                 </button>
             </div>
