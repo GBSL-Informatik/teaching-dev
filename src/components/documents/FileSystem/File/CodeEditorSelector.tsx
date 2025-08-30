e78549a551a093922151cf005fb43b4c4857c5a4
bh0fer <lebalz@outlook.com>
2025-08-30 15:14:30 +0200

    rm unused props

--- src/components/documents/FileSystem/File/CodeEditorSelector.tsx
+++ src/components/documents/FileSystem/File/CodeEditorSelector.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.scss';
 import { observer } from 'mobx-react-lite';
-import { useStore } from '@tdev-hooks/useStore';
 import Script from '@tdev-models/documents/Script';
 import CodeEditorComponent from '@tdev-components/documents/CodeEditor';
 import HtmlEditor from '@tdev-components/documents/CodeEditor/HtmlEditor';
