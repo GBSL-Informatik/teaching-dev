09b0867a7e844668d5827a90860a9b641afc2e8f
Silas Berger <silas.berger@outlook.com>
2025-09-02 11:24:19 +0200

    Add profile button hover dropdown for admins.

--- src/components/Navbar/LoginProfileButton/index.tsx
+++ src/components/Navbar/LoginProfileButton/index.tsx
@@ -3,32 +3,32 @@ import clsx from 'clsx';
 
 import styles from './styles.module.scss';
 import { observer } from 'mobx-react-lite';
-import { mdiAccountCircleOutline, mdiLogin } from '@mdi/js';
+import {
+    mdiAccountCircleOutline,
+    mdiAccountDetailsOutline,
+    mdiAccountSupervisorOutline,
+    mdiGroup,
+    mdiLogin,
+    mdiShieldAccountOutline
+} from '@mdi/js';
 import siteConfig from '@generated/docusaurus.config';
 import { useStore } from '@tdev-hooks/useStore';
 import Button from '@tdev-components/shared/Button';
 import useIsBrowser from '@docusaurus/useIsBrowser';
 import useIsMobileView from '@tdev-hooks/useIsMobileView';
 import LiveStatusIndicator from '@tdev-components/LiveStatusIndicator';
+import Popup from 'reactjs-popup';
 const { NO_AUTH } = siteConfig.customFields as { NO_AUTH?: boolean };
 
 const LoginButton = () => {
     return <Button href={'/login'} text="Login" icon={mdiLogin} color="primary" iconSide="left" />;
 };
 
-const LoginProfileButton = observer(() => {
-    const isBrowser = useIsBrowser();
+const ProfileButton = () => {
     const isMobile = useIsMobileView(502);
     const userStore = useStore('userStore');
     const sessionStore = useStore('sessionStore');
 
-    if (!isBrowser) {
-        return null;
-    }
-
-    if (!sessionStore.isLoggedIn && !NO_AUTH) {
-        return <LoginButton />;
-    }
     return (
         <div className={clsx(styles.profileButton, isMobile && styles.collapsed)}>
             {sessionStore.apiMode === 'api' ? (
@@ -58,6 +58,61 @@ const LoginProfileButton = observer(() => {
                 />
             )}
         </div>
+    );
+};
+
+const LoginProfileButton = observer(() => {
+    const isBrowser = useIsBrowser();
+    const sessionStore = useStore('sessionStore');
+    const userStore = useStore('userStore');
+
+    if (!isBrowser) {
+        return null;
+    }
+
+    if (!sessionStore.isLoggedIn && !NO_AUTH) {
+        return <LoginButton />;
+    }
+
+    if (!userStore.current?.isAdmin && !userStore.current?.isTeacher) {
+        return <ProfileButton />;
+    }
+
+    return (
+        <Popup
+            trigger={
+                <div>
+                    <ProfileButton />
+                </div>
+            }
+            on={['hover']}
+            closeOnDocumentClick
+            closeOnEscape
+        >
+            <div className={clsx(styles.adminNavPopup)}>
+                <Button
+                    href="/admin?panel=studentGroups"
+                    text="Lerngruppen"
+                    icon={mdiAccountSupervisorOutline}
+                    iconSide="left"
+                    className={clsx(styles.adminNavButton)}
+                />
+                <Button
+                    href="/admin?panel=accounts"
+                    text="Accounts"
+                    icon={mdiAccountDetailsOutline}
+                    iconSide="left"
+                    className={clsx(styles.adminNavButton)}
+                />
+                <Button
+                    href="/admin?panel=allowedActions"
+                    text="Erlaubte Aktionen"
+                    icon={mdiShieldAccountOutline}
+                    iconSide="left"
+                    className={clsx(styles.adminNavButton)}
+                />
+            </div>
+        </Popup>
     );
 });
 
