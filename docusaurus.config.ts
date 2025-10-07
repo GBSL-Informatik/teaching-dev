a62e2caf0b2a3cb9b8be64d7df21e65ae4b0b834
bh0fer <lebalz@outlook.com>
2025-10-07 18:03:15 +0200

    add announcement bar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -238,6 +238,9 @@ const config: Config = applyTransformers({
   ],
   themeConfig: {
     image: siteConfig.socialCard ?? 'img/social-card.jpg',
+    announcementBar: {
+      content: 'Aktuell keine API-Anbindung, siehe PR #246'
+    },
     navbar: {
       title: TITLE,
       logo: {
