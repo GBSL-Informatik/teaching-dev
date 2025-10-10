873a38dff0abdb535162dad946db93e04eca18df
bh0fer <lebalz@outlook.com>
2025-10-10 18:30:05 +0200

    cleanup

--- src/auth-client.ts
+++ src/auth-client.ts
@@ -2,14 +2,9 @@ import { createAuthClient } from 'better-auth/react';
 import { oneTimeTokenClient } from 'better-auth/client/plugins';
 import siteConfig from '@generated/docusaurus.config';
 interface AuthFields {
-    APP_URL: string;
     BACKEND_URL: string;
-    CLIENT_ID: string;
-    TENANT_ID: string;
-    API_URI: string;
 }
-export const { BACKEND_URL, CLIENT_ID, APP_URL, TENANT_ID, API_URI } =
-    siteConfig.customFields as any as AuthFields;
+export const { BACKEND_URL } = siteConfig.customFields as any as AuthFields;
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
