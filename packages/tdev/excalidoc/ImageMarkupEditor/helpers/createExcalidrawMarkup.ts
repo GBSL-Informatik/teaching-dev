7be30bffc8bf66ee7278cd37b603df6365b2b46a
bh0fer <lebalz@outlook.com>
2025-11-16 17:01:00 +0100

    refactor: let user decide about export quality, backgrounds and margins

--- packages/tdev/excalidoc/ImageMarkupEditor/helpers/createExcalidrawMarkup.ts
+++ packages/tdev/excalidoc/ImageMarkupEditor/helpers/createExcalidrawMarkup.ts
@@ -11,7 +11,8 @@ import {
     EXCALIDRAW_BACKGROUND_IMAGE,
     EXCALIDRAW_MAX_WIDTH,
     CustomData,
-    EXCALIDRAW_STANDALONE_DRAWING_ID
+    EXCALIDRAW_STANDALONE_DRAWING_ID,
+    EXCALIDRAW_STANDALONE_DRAWING_RECTANGLE
 } from './constants';
 import { withoutMetaElements } from './getElementsFromScene';
 import { getBoundingRect } from './getBoundingRect';
@@ -68,6 +69,8 @@ const handleStandaloneDrawing = (excalidrawState: ExcalidrawInitialDataState): E
 
     (excalidrawState.elements as ExcalidrawElement[]).splice(standaloneDrawingIdx, 1, {
         ...excalidrawState.elements[standaloneDrawingIdx],
+        strokeWidth: EXCALIDRAW_STANDALONE_DRAWING_RECTANGLE.strokeWidth,
+        strokeColor: EXCALIDRAW_STANDALONE_DRAWING_RECTANGLE.strokeColor,
         width,
         height,
         x: x,
@@ -82,13 +85,19 @@ export const updateRectangleDimensions = (
     if (!excalidrawState.elements) {
         return excalidrawState;
     }
-    const isStandaloneDrawing = excalidrawState.elements.some(
-        (e) => e.id === EXCALIDRAW_STANDALONE_DRAWING_ID
-    );
-    if (isStandaloneDrawing) {
+    const backgroundImage = excalidrawState.elements.find((e) => e.id === EXCALIDRAW_BACKGROUND_IMAGE_ID);
+    if (!backgroundImage) {
+        const isStandaloneDrawing = excalidrawState.elements.some(
+            (e) => e.id === EXCALIDRAW_STANDALONE_DRAWING_ID
+        );
+        if (!isStandaloneDrawing) {
+            // add standalone markup drawing rectangle
+            (excalidrawState.elements as ExcalidrawElement[]).splice(0, 0, {
+                ...EXCALIDRAW_STANDALONE_DRAWING_RECTANGLE
+            });
+        }
         return handleStandaloneDrawing(excalidrawState);
     }
-    const backgroundImage = excalidrawState.elements.find((e) => e.id === EXCALIDRAW_BACKGROUND_IMAGE_ID);
     if (!backgroundImage || backgroundImage.type !== 'image') {
         return excalidrawState;
     }
