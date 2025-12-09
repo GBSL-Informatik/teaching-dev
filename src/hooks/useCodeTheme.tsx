8f4329a42f422609976446316516530ffda98e6d
bh0fer <lebalz@outlook.com>
2025-12-09 09:09:23 +0100

    feature: themed code editors

---
+++ src/hooks/useCodeTheme.tsx
@@ -0,0 +1,26 @@
+import { StorageKey } from '@tdev-stores/utils/Storage';
+import useLocalStorage from './useLocalStorage';
+import { useColorMode } from '@docusaurus/theme-common';
+
+const AceThemeMap = {
+    light: 'xcode',
+    dark: 'dracula'
+};
+
+const useCodeTheme = () => {
+    const { colorMode } = useColorMode();
+    const [value, setValue] = useLocalStorage<'light' | 'dark' | 'system'>(
+        StorageKey.CodeTheme,
+        'system',
+        true
+    );
+    const codeTheme = value ?? 'system';
+    return {
+        codeTheme: codeTheme,
+        setCodeTheme: setValue,
+        colorMode,
+        aceTheme: codeTheme === 'system' ? AceThemeMap[colorMode] : AceThemeMap[codeTheme]
+    } as const;
+};
+
+export default useCodeTheme;
