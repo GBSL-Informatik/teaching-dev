745b836f37b0a5fd7adb4311c786f4499326727b
bh0fer <lebalz@outlook.com>
2026-01-11 19:03:29 +0100

    only require ComWorkers on client

--- packages/tdev/pyodide-script/stores/PyodideStore.ts
+++ packages/tdev/pyodide-script/stores/PyodideStore.ts
@@ -3,12 +3,17 @@ import * as Comlink from 'comlink';
 import type { SquareWorker, WorkerApi } from '../workers/square.worker';
 import ViewStore from '@tdev-stores/ViewStores';
 import { PyWorker, PyWorkerApi } from '../workers/pyodide.worker';
-const ComSquareWorker = Comlink.wrap<WorkerApi>(
-    new Worker(new URL('../workers/square.worker.ts', import.meta.url), { type: 'module' })
-);
-const ComPyWorker = Comlink.wrap<PyWorkerApi>(
-    new Worker(new URL('../workers/pyodide.worker.ts', import.meta.url), { type: 'module' })
-);
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+const ComSquareWorker = ExecutionEnvironment.canUseDOM
+    ? Comlink.wrap<WorkerApi>(
+          new Worker(new URL('../workers/square.worker.ts', import.meta.url), { type: 'module' })
+      )
+    : null;
+const ComPyWorker = ExecutionEnvironment.canUseDOM
+    ? Comlink.wrap<PyWorkerApi>(
+          new Worker(new URL('../workers/pyodide.worker.ts', import.meta.url), { type: 'module' })
+      )
+    : null;
 
 export default class PyodideStore {
     viewStore: ViewStore;
@@ -25,6 +30,9 @@ export default class PyodideStore {
     }
 
     async initialize() {
+        if (!ComSquareWorker || !ComPyWorker) {
+            return;
+        }
         const libs = await Promise.all([new ComSquareWorker(), new ComPyWorker()]);
         runInAction(() => {
             this.squareWorker = libs[0];
