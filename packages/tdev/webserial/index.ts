b9acd50b4db2c0437c52fe6a4003708dd1a404ff
bh0fer <lebalz@outlook.com>
2026-03-02 08:34:34 +0100

    remove unused export

--- packages/tdev/webserial/index.ts
+++ packages/tdev/webserial/index.ts
@@ -1,9 +1,5 @@
 import type WebserialStore from './stores/WebserialStore';
 
-export const tdevWebserial = () => {
-    console.log('Hello from tdev-webserial!');
-};
-
 declare module '@tdev-api/document' {
     export interface ViewStoreTypeMapping {
         ['webserialStore']: WebserialStore;
