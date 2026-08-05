222c264213b090d105e25c1dbb70beb047e9e917
bh0fer <lebalz@outlook.com>
2026-08-05 13:29:21 +0200

    finalize migration

--- packages/tdev/material-sync/src/migrate_tdev/migrations/2026-08-04-include-presentation-mode.ts
+++ packages/tdev/material-sync/src/migrate_tdev/migrations/2026-08-04-include-presentation-mode.ts
@@ -1,8 +1,8 @@
 import { MigrationRunner } from '../constants';
 import { execa } from 'execa';
-import { updateTdevConfig } from '../helpers/loadFile';
-import { ensureTdevConfig } from '../helpers/actions';
-import { writeUpdateTdevConfig } from '../helpers/writeFile';
+import { packageJson, updateTdevConfig } from '../helpers/loadFile';
+import { ensureTdevConfig, modifyPackages } from '../helpers/actions';
+import { writePackageJson, writeUpdateTdevConfig } from '../helpers/writeFile';
 
 const migrate: MigrationRunner = async (root, apiMode, managed, timestamp): Promise<void> => {
     console.log('Starting TDEV migration: ', root);
@@ -23,6 +23,27 @@ const migrate: MigrationRunner = async (root, apiMode, managed, timestamp): Prom
 
     await $`git add .`;
     await $`git commit -m ${'[tdev] include presentation mode.'}`;
+
+    // package.json
+    const pkg = await packageJson(root);
+    modifyPackages(pkg, {
+        dependencies: {
+            'ace-builds': '^1.44.0',
+            'better-auth': '^1.6.26',
+            mobx: '^7.0.0',
+            'mobx-react-lite': '^5.0.0',
+            'react-ace': '^15.0.0'
+        }
+    });
+    await writePackageJson(root, pkg);
+    await $`rm -rf node_modules`;
+    await $`rm yarn.lock`;
+    await $`yarn install`;
+    await $`yarn format`;
+
+    await $`git add .`;
+    await $`git commit -m ${'[tdev] update dependencies with mobx@7.'}`;
+
     await $`git checkout main`;
     await $`git merge ${branchName}`;
     await $`git branch -d ${branchName}`;
