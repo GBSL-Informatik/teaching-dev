cbdf6df2049c71b8570a8401a7e35f98e2707d86
bh0fer <lebalz@outlook.com>
2026-08-23 12:37:37 +0200

    add option to pass a when function

--- src/components/documents/Assessable/Feedback/Hint/index.tsx
+++ src/components/documents/Assessable/Feedback/Hint/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { useDocument } from '@tdev-hooks/useContextDocument';
-import { AssessableType } from '@tdev-api/document';
+import type { AssessableType, TypeModelMapping } from '@tdev-api/document';
 import { Correctness } from '@tdev-models/documents/Assessable/iAssessable';
 import Alert, { type AlertType } from '@tdev-components/shared/Alert';
 
 type When = Correctness | 'assessed' | 'unassessed';
+type WhenFunction<T extends AssessableType> = (
+    doc: TypeModelMapping[T]
+) => { show: boolean; color: AlertType; children?: React.ReactNode; className?: string } | null;
 
-interface Props {
-    children?: React.ReactNode;
-    when: When | When[];
-    not: When | When[];
+interface Props<T extends AssessableType = AssessableType> {
+    when: When | When[] | WhenFunction<T>;
+    not?: When | When[];
     noWrap?: boolean;
     color?: AlertType;
+    children?: React.ReactNode;
     className?: string;
 }
 
@@ -25,22 +28,41 @@ const ColorMapping: { [key in When]: AlertType } = {
     unassessed: 'info'
 };
 
-const AlertHint = observer((props: Omit<Props, 'when'> & { when: When }) => {
+const AlertHint = observer((props: Omit<Props, 'when'> & { when?: When }) => {
     if (props.noWrap) {
         return <>{props.children}</>;
     }
 
     return (
-        <Alert type={props.color ?? ColorMapping[props.when]} className={props.className}>
+        <Alert
+            type={props.color ?? (props.when ? ColorMapping[props.when] : undefined)}
+            className={props.className}
+        >
             {props.children}
         </Alert>
     );
 });
 
-const Hint = observer(<T extends AssessableType>(props: Props) => {
+const Hint = observer(<T extends AssessableType>(props: Props<T>) => {
     const doc = useDocument<T>();
+    const whenFn = typeof props.when === 'function' ? props.when : null;
+    if (whenFn) {
+        const res = whenFn(doc);
+        if (!res || !res.show) {
+            return null;
+        }
+        return (
+            <AlertHint
+                {...props}
+                when={Correctness.NA}
+                color={res.color}
+                children={res.children ?? props.children}
+                className={res.className ?? props.className}
+            />
+        );
+    }
     const when = Array.isArray(props.when) ? new Set(props.when) : new Set([props.when]);
-    const not = Array.isArray(props.not) ? new Set(props.not) : new Set([props.not]);
+    const not = Array.isArray(props.not) ? new Set(props.not) : new Set(props.not ? [props.not] : []);
     if (!doc.isAssessed) {
         if (when.has('unassessed')) {
             return <AlertHint {...props} when="unassessed" />;
